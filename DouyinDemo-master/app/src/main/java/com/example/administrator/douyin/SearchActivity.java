fc17db689ec3f45fa164b1eae35f3caa099798b4
zhangxih <1262806950@qq.com>
2020-07-15 18:05:15 +0800

    完成搜索界面、加入视频分类功能

--- DouyinDemo-master/app/src/main/java/com/example/administrator/douyin/SearchActivity.java
+++ DouyinDemo-master/app/src/main/java/com/example/administrator/douyin/SearchActivity.java
@@ -2,15 +2,20 @@ package com.example.administrator.douyin;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.Serializable;
@@ -21,7 +26,10 @@ public class SearchActivity extends AppCompatActivity {
     private EditText keyword;
     private int[] imgs = {R.mipmap.img_video_3, R.mipmap.img_video_2, R.mipmap.img_video_1};
     private int[] videos = {R.raw.video_3, R.raw.video_2, R.raw.video_1};
+    private String[] describes = {"第一个视频简介","阿巴阿巴阿巴阿巴","2333333333"};
     private Context p = this;
+    private CheckBox[] cbs = new CheckBox[11];
+    private String kinds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +37,38 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         final LinearLayout layout = (LinearLayout) this.findViewById(R.id.layout);
+        cbs[0]=this.findViewById(R.id.music_cb);
+        cbs[1]=this.findViewById(R.id.movie_cb);
+        cbs[2]=this.findViewById(R.id.society_cb);
+        cbs[3]=this.findViewById(R.id.game_cb);
+        cbs[4]=this.findViewById(R.id.food_cb);
+        cbs[5]=this.findViewById(R.id.child_cb);
+        cbs[6]=this.findViewById(R.id.live_cb);
+        cbs[7]=this.findViewById(R.id.PE_cb);
+        cbs[8]=this.findViewById(R.id.culture_cb);
+        cbs[9]=this.findViewById(R.id.fashion_cb);
+        cbs[10]=this.findViewById(R.id.science_cb);
 
         SearchButton = (Button) findViewById(R.id.search);
         keyword = (EditText) findViewById(R.id.keyword);
         SearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //在这里根据搜索条件获取视频，并设置视频和封面的路径数组
+                kinds="";
+                for(int i=0;i<11;i++){
+                    if(cbs[i].isChecked()){
+                        kinds = kinds + cbs[i].getText() + " ";
+                    }
+                }
+                Toast.makeText(p,kinds,Toast.LENGTH_SHORT).show();
+                //在这里根据搜索条件(keyword和kinds)获取视频，并设置视频和封面的路径数组以及视频描述
 
 
 
-                
-                DisplayMetrics dm = new DisplayMetrics();
-                getWindowManager().getDefaultDisplay().getMetrics(dm);
-                int width = dm.widthPixels;
-                int height = dm.heightPixels;
+
 
                 ImageButton Btn[] = new ImageButton[imgs.length];
+                TextView Txt[] = new TextView[imgs.length];
                 int j = -1;
                 for (int i=0; i<Btn.length; i++) {
                     Btn[i] = new ImageButton(p);
@@ -53,11 +76,16 @@ public class SearchActivity extends AppCompatActivity {
                     Btn[i].setImageResource(imgs[i]);
                     Btn[i].setBackgroundColor(00000000);
                     Btn[i].setScaleType(ImageView.ScaleType.FIT_START);
+                    Txt[i] = new TextView(p);
+                    Txt[i].setText(describes[i]);
+                    Txt[i].setTextSize(40);
+                    Txt[i].setTextColor(Color.rgb(255, 255, 255));
                     layout.addView(Btn[i]); //将按钮放入layout组件
+                    layout.addView(Txt[i]);
                 }
 
                 for (int k = 0; k <= Btn.length-1; k++) {
-                    //这里不需要findId，因为创建的时候已经确定哪个按钮对应哪个Id
+
                     Btn[k].setTag(k);    //为按钮设置一个标记，来确认是按下了哪一个按钮
 
                     Btn[k].setOnClickListener(new Button.OnClickListener() {
